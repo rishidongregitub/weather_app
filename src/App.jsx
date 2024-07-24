e424ebffb583893a411ce6b48ba581600afc9a55
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-07-24 21:40:32 +0530

    Forecast,TempAndDetail,TimeAndLocation Component creation with some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Inputs from "./Components/Inputs";
+import TempAndDetail from "./Components/TempAndDetail";
+import TimeAndLocation from "./Components/TimeAndLocation";
 import TopButtons from "./Components/TopButtons";
+import Forecast from "./Components/Forecast";
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
       <Inputs/>
+      <TimeAndLocation/>
+      <TempAndDetail/>
+      <Forecast/>
+      <Forecast/>
     </div>
   );
 }
---
+++ src/Components/TempAndDetail.jsx
@@ -0,0 +1,90 @@
+import { FaThermometerEmpty } from "react-icons/fa";
+import { BiSolidDropletHalf } from "react-icons/bi";
+import { FiWind } from "react-icons/fi";
+import { GiSunrise, GiSunset } from "react-icons/gi";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+const TempAndDetail = () => {
+  const verticalDetails = [
+    {
+      id: 1,
+      Icon: FaThermometerEmpty,
+      title: "Real Feel",
+      value: "22°",
+    },
+    {
+      id: 2,
+      Icon: BiSolidDropletHalf,
+      title: "Humidity",
+      value: "36%",
+    },
+    {
+      id: 3,
+      Icon: FiWind,
+      title: "Wind Speed",
+      value: "11 km/h",
+    },
+  ];
+  const horizontalDetails = [
+    {
+      id: 1,
+      Icon: GiSunrise,
+      title: "Sun Rise",
+      value: "05:00 AM",
+    },
+    {
+      id: 2,
+      Icon: GiSunset,
+      title: "Sun Set",
+      value: "06:30",
+    },
+    {
+      id: 3,
+      Icon: MdKeyboardArrowUp,
+      title: "High",
+      value: "37°",
+    },
+    {
+      id: 4,
+      Icon: MdKeyboardArrowDown,
+      title: "Low",
+      value: "12°",
+    },
+  ];
+  return (
+    <>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>Rain</p>
+      </div>
+      <div className="flex flex-row items-center justify-between py-3">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="weather icon"
+          className="w-20"
+        />
+        <p className="text-5xl">34°</p>
+        <div className="flex flex-col space-y-3 items-start">
+          {verticalDetails.map(({ id, title, Icon, value }) => (
+            <div
+              key={id}
+              className="flex font-light text-sm items-center justify-center"
+            >
+              <Icon size={18} className="mr-1" />
+              {title} : <span className="font-medium ml-1">{value}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-10 text-sm py-3">
+        {horizontalDetails.map(({ id, title, Icon, value }) => (
+          <div key={id} className="flex flex-row items-center">
+            <Icon size={30}/>
+            <p className="font-light ml-1">{`${title}:`}</p>
+            <span className="font-medium ml-1">{value}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TempAndDetail;
